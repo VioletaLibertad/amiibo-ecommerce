c18a4aa38fbac42f62c18f14fa85a3a6050966ae
Violeta Libertad <morada.libertad@gmail.com>
2020-06-17 22:33:29 -0400

    add and remove elements from cart ready

--- src/components/BaseCard/index.js
+++ src/components/BaseCard/index.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './style.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { addToCart, removeFromCart, getCart } from '../../redux/actions';
 
 function BaseCard(props) {
+  const [productAdded, setProductAdded] = useState(false);
+  const dispatch = useDispatch();
 
+  const cart = useSelector(state => state.cart);
   let cardData = props.data;
+  let itemInCart = cart.find(item => item.tail === cardData.tail);
+
+  useEffect(() => {
+    if (itemInCart) {
+      setProductAdded(true);
+    } else {
+      setProductAdded(false);
+    }
+  });
 
   return(
     <Card className="baseCard">
-      <Card.Img className="cardImg" variant="top" src={cardData.image} />
+      <Card.Img loading="lazy" className="cardImg" variant="top" src={cardData.image} />
       <Card.Body>
         <Card.Title>{cardData.name}</Card.Title>
         <Card.Text>{cardData.amiiboSeries}</Card.Text>
         <Card.Text>{cardData.type}</Card.Text>
       </Card.Body>
       <Card.Footer>
-        <Button variant="primary" block>Agregar al carrito</Button>
+        {!productAdded
+          ? <Button variant="primary" 
+            onClick={() => {
+              dispatch(addToCart(cardData));
+              setProductAdded(true)}} block>
+                Agregar al carrito
+            </Button>
+          : <Button variant="danger" 
+            onClick={() => {
+              dispatch(removeFromCart(cardData));
+              setProductAdded(false)}} block>
+                Remover del carrito
+            </Button>
+        }
       </Card.Footer>
     </Card>
   );
--- src/components/CartModal/index.js
+++ src/components/CartModal/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './style.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
 import { checkout } from '../../routes';
+import { removeFromCart } from '../../redux/actions';
+
 
 function CartModal(props) {
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state => state.cart);
 
   return(
     <Modal
@@ -15,16 +22,36 @@ function CartModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Tus futuros amiibos
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <Table borderless responsive>
+          <thead>
+            <tr>
+              <th>Personaje</th>
+              <th>Amiibo Serie</th>
+              <th>Tipo</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map(elem =>
+              <tr key={elem.tail}>
+                <td>{elem.character}</td>
+                <td>{elem.amiiboSeries}</td>
+                <td>{elem.type}</td>
+                <td>
+                <Button size="sm" variant="danger" 
+                onClick={() => dispatch(removeFromCart(elem))} block>
+                  Remover
+                </Button>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+
       </Modal.Body>
       <Modal.Footer>
         <Link to={checkout} onClick={props.onHide}>
