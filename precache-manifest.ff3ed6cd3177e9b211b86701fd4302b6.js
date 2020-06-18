4cba13e85ec7bf0f757e05b6a7ea43eac3446584
Violeta Libertad <morada.libertad@gmail.com>
2020-06-18 01:14:44 -0400

    Updates

---
+++ precache-manifest.ff3ed6cd3177e9b211b86701fd4302b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18070a155b758ce5a07fcbdf1b846ec",
+    "url": "/amiibo-ecommerce/index.html"
+  },
+  {
+    "revision": "b534dc786d17ed1e4079",
+    "url": "/amiibo-ecommerce/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ef2213bfc547430638a",
+    "url": "/amiibo-ecommerce/static/css/main.89ad5625.chunk.css"
+  },
+  {
+    "revision": "b534dc786d17ed1e4079",
+    "url": "/amiibo-ecommerce/static/js/2.7ad9e978.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/amiibo-ecommerce/static/js/2.7ad9e978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef2213bfc547430638a",
+    "url": "/amiibo-ecommerce/static/js/main.1bbf58a4.chunk.js"
+  },
+  {
+    "revision": "9797591220809557f713",
+    "url": "/amiibo-ecommerce/static/js/runtime-main.2a9ac712.js"
+  }
+]);
